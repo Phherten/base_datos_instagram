fa2a6d38b2bfb580be89b43e4ce267b6bdd9013a
Phherten <phigueraherten@gmail.com>
2022-05-16 18:29:13 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -7,7 +7,55 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    user_id = Column (Integer, ForeignKey("user.id"))
+    user = relationship(User)
+
+class Follower(Base):
+    __tablename__ = "follower"
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column (Integer, ForeignKey("user.id"))
+    user_to_id = Column (Integer, ForeignKey("user.id"))
+    user = relationship(User)
+
+class Media(Base):
+    __tablename__ = "media"
+    id = Column(Integer, primary_key=True)
+    type = Column(Integer)
+    url = Column(String(250), nullable=False)
+    post_id = Column (Integer, ForeignKey("post.id"))
+    post = relationship(Post)
+
+class Comment(Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column (Integer, ForeignKey("user.id"))
+    post_id = Column (Integer, ForeignKey("post.id"))
+    user = relationship(User)
+    post = relationship(Post)
+    
+
+
+
+
+
+
+
+
+
+
+"""
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
@@ -25,9 +73,8 @@ class Address(Base):
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
-
-    def to_dict(self):
-        return {}
+"""
+   
 
 ## Draw from SQLAlchemy base
 try:
